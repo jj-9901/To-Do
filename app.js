29f6e1ec08469c5afa6c1d5e8cdd729fc9e0af2f
jj-9901 <178759356+jj-9901@users.noreply.github.com>
2025-02-05 18:58:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const input= document.getElementById("input");
+const items= document.querySelector(".items");
+const button= document.querySelector("button");
+
+input.addEventListener("keydown",function(event){
+    if(event.key==="Enter"){
+        addElement();
+    }
+  }
+);
+
+button.addEventListener("click",function(event){
+        addElement();
+  }
+);
+
+function addElement(){
+    if((input.value).trim().length===0){
+        alert("enter something");
+    }
+    else{
+        const newDiv= document.createElement("div");
+        newDiv.classList.add("item");
+        newDiv.innerHTML=`<i class="fa-regular fa-square"></i><span class="text">${input.value.trim()}</span>
+                    <i class="fa-solid fa-check"></i></i><span class="x">x</span>`;
+        items.appendChild(newDiv);
+        saveData();
+    }
+    input.value="";
+}
+
+items.addEventListener("click", function (event) {
+    if (event.target.classList.contains("x")) { // Check if clicked element is the 'x'
+        event.target.parentElement.remove(); // Remove the parent (the item)
+    }
+    // text|| fa-square opacity
+    // else{
+    //     event.target.parentElement.classList.toggle("checked");
+    // }
+
+    else if (event.target.classList.contains("text") || event.target.classList.contains("fa-square")) {
+        event.target.closest(".item").classList.toggle("checked");
+    }
+    else if(event.target.classList.contains("item")){
+        event.target.classList.toggle("checked");
+    }
+    saveData();
+});
+
+
+function saveData(){
+    localStorage.setItem("toDoListData",items.innerHTML);
+}
+
+
+const nameIn = document.querySelector(".name");
+nameIn.addEventListener("keydown", function(event){
+    saveName();
+});
+
+
+function showData(){
+    items.innerHTML = localStorage.getItem("toDoListData");
+    nameIn.value = localStorage.getItem("Name");
+}
+
+
+function saveName(){
+    localStorage.setItem("Name",nameIn.value);
+}
+
+showData();
